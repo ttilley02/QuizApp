9fad38fe32f82962917c502fc1ff089d1eec7ffc
ttilley02 <ttilley02@gmail.com>
2020-02-10 18:38:15 -0500

    replit changes

---
+++ index.js
@@ -0,0 +1,12 @@
+/*function render() {
+    if (store.view === 'start') {
+      $('.intro').show();
+      $('.quiz').hide();
+      $('.status').hide();
+    } else if (store.view === 'quiz') {
+      $('.intro').hide();
+      $('.quiz').show();
+      $('.status').show();
+    }
+  } 
+  */
